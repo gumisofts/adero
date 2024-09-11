f6c93b31a2e22a6c7e6cb94c0f37082b3c39bb42
nurad <nuradhussen082@gmail.com>
2024-09-11 10:52:15 +0300

    edu started

--- core/urls.py
+++ core/urls.py
@@ -4,18 +4,22 @@ from django.conf.urls.static import static
 from django.conf import settings
 from app.views import *
 
-urlpatterns = [
-    path("", home),
-    path("about/", about),
-    path("services/", services),
-    path("careers/", career),
-    path("faq/", faq),
-    path("contact_us/", contact_us),
-    path("apply_job/<int:id>", apply_job),
-    path("services/software_services/", softawre_service),
-    path("services/education_consultancy/", education_consultancy),
-    path("services/training_and_courses/", training_and_courses),
-    path("verifytoken/", verfytoken),
-    path("admin/", admin.site.urls),
-    path("test/", test),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns = (
+    [
+        path("", home),
+        path("about/", about),
+        path("services/", services),
+        path("careers/", career),
+        path("faq/", faq),
+        path("contact_us/", contact_us),
+        path("apply_job/<int:id>", apply_job),
+        path("services/software_services/", softawre_service),
+        path("services/education_consultancy/", education_consultancy),
+        path("services/training_and_courses/", training_and_courses),
+        path("verifytoken/", verfytoken),
+        path("admin/", admin.site.urls),
+        path("test/", test),
+    ]
+    + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    + static(settings.STATIC_URL, document_root="static")
+)
